1bad34d46697a40be1f9ceb2c1c6a0f2f70328fd
Donal Gaspard <lyr.x.informatique@gmail.com>
2024-05-13 10:28:20 +0200

    add fullName to user utils

--- src/components/userDashboard/User.tsx
+++ src/components/userDashboard/User.tsx
@@ -1,17 +1,5 @@
 import React from 'react';
-
-const fullName = (firstname?: string , lastname?: string | null ) => {
-  let firstnameCapitalize;
-  let lastnameCapitalize;
-  if (firstname) {
-     firstnameCapitalize =  firstname.charAt(0).toUpperCase() + firstname.slice(1);
-  }
-  if (lastname) {
-      lastnameCapitalize =  lastname.charAt(0).toUpperCase() + lastname.slice(1);
-  }
-
-    return `${firstnameCapitalize} ${lastnameCapitalize}`;
-}
+import {fullName} from "./user.utils.ts";
 
 interface Props{
     userId: string | number;
--- src/components/userDashboard/user.utils.ts
+++ src/components/userDashboard/user.utils.ts
@@ -2,3 +2,17 @@
 export const getInitial = ( lastName: string, firstName?: string) =>{
   return `${firstName?.charAt(0)}${lastName.charAt(0)}`
 }
+
+
+export const fullName = (firstname?: string , lastname?: string | null ) => {
+  let firstnameCapitalize;
+  let lastnameCapitalize;
+  if (firstname) {
+    firstnameCapitalize =  firstname.charAt(0).toUpperCase() + firstname.slice(1);
+  }
+  if (lastname) {
+    lastnameCapitalize =  lastname.charAt(0).toUpperCase() + lastname.slice(1);
+  }
+
+  return `${firstnameCapitalize} ${lastnameCapitalize}`;
+}
